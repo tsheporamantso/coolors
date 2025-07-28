a8850350f8b9e51dd594737c6e306febbb44f453
tsheporamantso <tshepo.ramantso@outlook.com>
2025-07-28 14:31:57 +0200

    Refactor ColorsList component to accept colors as props and render SingleColor components

--- src/components/ColorsList.jsx
+++ src/components/ColorsList.jsx
@@ -1,7 +1,14 @@
-import React from "react";
+import { nanoid } from "nanoid";
+import SingleColor from "./SingleColor";
 
-const ColorsList = () => {
-  return <div>ColorsList</div>;
+const ColorsList = ({ colors }) => {
+  return (
+    <section className="colors">
+      {colors.map((color, index) => {
+        return <SingleColor key={nanoid()} color={color} index={index} />;
+      })}
+    </section>
+  );
 };
 
 export default ColorsList;
