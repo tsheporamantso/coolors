a6b233f920c24f07a09497d4b8337be04225eae2
tsheporamantso <tshepo.ramantso@outlook.com>
2025-07-28 14:32:03 +0200

    Implement SingleColor component to display color details with dynamic styling

--- src/components/SingleColor.jsx
+++ src/components/SingleColor.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 
-const SingleColor = () => {
-  return <div>SingleColor</div>;
+const SingleColor = ({ color, index }) => {
+  const { hex, weight } = color;
+  return (
+    <article
+      className={index > 10 ? "color-light color" : "color"}
+      style={{ background: `#${hex}` }}
+    >
+      <p className="color-value">#{hex}</p>
+      <p className="color-percentage">{weight}%</p>
+    </article>
+  );
 };
 
 export default SingleColor;
