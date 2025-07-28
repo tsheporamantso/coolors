6881fc4a7c98b2bf4b58d5353df44476371929dc
tsheporamantso <tshepo.ramantso@outlook.com>
2025-07-28 17:12:10 +0200

    Integrate color addition functionality with toast notifications in App component

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,28 @@ import { useState } from "react";
 import ColorsList from "./components/ColorsList";
 import Form from "./components/Form";
 import Values from "values.js";
+import { ToastContainer, toast } from "react-toastify";
 
-console.log(new Values("#f15025").all(10));
+// console.log(new Values("#f15025").all(10));
 
 function App() {
   const [colors, setColors] = useState(new Values("#f15025").all(10));
 
+  const addColor = (color) => {
+    try {
+      const newColor = new Values(color).all(10);
+      setColors(newColor);
+      toast.success("Colors loaded successfully!");
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
+
   return (
     <main>
-      <Form />
+      <Form addColor={addColor} />
       <ColorsList colors={colors} />
+      <ToastContainer position="top-center" />
     </main>
   );
 }
