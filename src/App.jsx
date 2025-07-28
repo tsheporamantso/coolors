674fd4d44ab2f2d7eeb1347648225fe28b868b11
tsheporamantso <tshepo.ramantso@outlook.com>
2025-07-28 14:31:51 +0200

    Add color state management and pass colors to ColorsList component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
+import { useState } from "react";
 import ColorsList from "./components/ColorsList";
 import Form from "./components/Form";
+import Values from "values.js";
+
+console.log(new Values("#f15025").all(10));
+
 function App() {
+  const [colors, setColors] = useState(new Values("#f15025").all(10));
+
   return (
     <main>
       <Form />
-      <ColorsList />
+      <ColorsList colors={colors} />
     </main>
   );
 }
